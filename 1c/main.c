3efe94c84e4b0b0d4de442c08ce490d711ced1f4
weilern1121 <37665993+weilern1121@users.noreply.github.com>
2018-05-21 16:41:14 +0300

    added 1c, 1d, 1e. still need to update README

---
+++ 1c/main.c
@@ -0,0 +1,338 @@
+#include <stdio.h>
+#include <linux/limits.h>
+#include <unistd.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include "line_parser.h"
+#include "job_control.h"
+
+//#include "line_parser.c"
+
+int exit_error_free(char* msg, job* job_list){
+    perror(msg);
+    free_job_list(&job_list);
+    exit(EXIT_FAILURE);
+}
+int exit_error(char* msg){
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void sig_handler(int sig){
+    //0a- interrupted -> ignored message
+    if (sig == SIGQUIT  || sig==SIGCHLD)
+        // printf("The interrupted signal was ignored by- %s\n",strsignal(sig));
+        //0c- stop signals ->ignored message for now
+        /*if(sig==SIGTTIN ||sig==SIGTTOU || sig==SIGTSTP)
+            printf("The stop signal was ignored by- %s\n",strsignal(sig));*/
+        fflush(stdout);
+    //exit(EXIT_SUCCESS);
+}
+
+void set_default_signals(){
+    signal(SIGQUIT,SIG_DFL);
+    signal(SIGCHLD,SIG_DFL);
+    signal(SIGTTIN,SIG_DFL);
+    signal(SIGTTOU,SIG_DFL);
+    signal(SIGTSTP,SIG_DFL);
+}
+//single execute- task5 ->NOT USED in task 6
+int single_execute(cmd_line *line){
+    int inputStream,outputStream;
+    //check the input and output fields before execute
+    if(line->input_redirect!=NULL) {
+        inputStream = open(line->input_redirect, O_RDONLY, 0);
+        if(inputStream<0) {
+            exit_error("inputStream error");
+        }
+        dup2(inputStream,0);
+    }
+    if(line->output_redirect!=NULL) {
+        outputStream = open(line->output_redirect, O_TRUNC | O_CREAT | O_WRONLY, 0644);
+        if(outputStream<0) {
+            exit_error("outputStream error");
+        }
+        dup2(outputStream,1);
+    }
+    int output=execvp(line->arguments[0],line->arguments);
+    //close streaming ->for each son
+    if(line->output_redirect!=NULL)
+        close( outputStream);
+    if(line->input_redirect!=NULL)
+        close(inputStream);
+
+    // -1 ->error
+    return output;
+}
+//double execute - task5 ->NOT USED in task 6
+int pipeExecute(cmd_line *line){
+    int fd[2],child1,child2;
+    //fd[0]-> for using read end    fd[1]-> for using write end
+
+    //#1
+    if(pipe(fd)==-1){
+        perror("pipe(fd)1 error");
+        exit(EXIT_FAILURE);
+    }
+    //#2
+    child1 = fork();
+    //#3
+    if (child1 == 0) {
+        close(1);
+        dup(fd[1]);
+        close(fd[1]);
+        if(single_execute(line)==-1){
+            perror("execvp1 error");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    //#4
+    close(fd[1]);
+    //#5-6
+    child2 = fork();
+    if (child2 == 0) {
+        close(0);
+        dup(fd[0]);
+        close(fd[0]);
+        if(single_execute(line->next)==-1){
+            perror("execvp2 error");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    //#7
+    close(fd[0]);
+    //#8
+    waitpid(child1, 0, 0);
+    waitpid(child2, 0, 0);
+    exit(EXIT_SUCCESS);
+}
+
+int execute (cmd_line *line, int* left_pipe, int* right_pipe , int* first_child_pid){
+    int fd[2],child1,result=0, status;
+    pid_t child_pid;
+
+    child1=fork();
+    //child part
+    if(child1==0){
+        set_default_signals();
+        if(right_pipe && line->next){
+            close(1);
+            dup(right_pipe[1]);
+            close(right_pipe[1]);
+        }
+        if(left_pipe){
+            close(0);
+            dup(left_pipe[0]);
+            close(left_pipe[0]);
+        }
+        result=single_execute(line);
+        waitpid(child_pid,&status,0);
+        exit(EXIT_SUCCESS);
+    }
+    //parent part
+    if(right_pipe)
+        close(right_pipe[1]);
+    if(left_pipe){
+        //TODO- need to free the pipe
+        close(left_pipe[0]);
+    }
+    //if there is a new command- create new pipe are Recursively call the same func
+    if(line->next){
+        if(pipe(fd)==-1)
+            exit_error("pipe(fd)1 error");
+        result=execute(line->next,right_pipe,fd,NULL);
+    }
+    return result;
+}
+//purpose:   wrapper function will handle the initial call and waiting for the last child
+int initial_execute(cmd_line *line, pid_t *child_pid){
+    int *right_pipe=NULL;
+    int fd[2];
+    if(line->next){
+        if(pipe(fd)==-1)
+            exit_error("pipe(fd)1 error");
+        else
+            right_pipe=fd;
+    }
+    return execute(line,NULL,right_pipe,child_pid);
+}
+
+/*
+int MultiPipeExecute(cmd_line *line, job* cur_job , job* job_list, int last_child_pid){
+    int fd[2],child1,child2;
+    //fd[0]-> for using read end    fd[1]-> for using write end
+
+    //#1
+    if(pipe(fd)==-1){
+        exit_error("pipe(fd)1 error");
+    }
+    //#2
+    child1 = fork();
+    //if true -> the pid of all processes and the job.pgid equals to the pid of this process
+    if(last_child_pid<0){
+        last_child_pid=getpid();
+        cur_job->pgid=last_child_pid;
+    }
+    //#3
+    if (child1 == 0) {
+        //child1 process
+        set_default_signals();
+        pid_t child_pid=getpid();
+        //setpgid(child_pid,child_pid);
+        setpgid(getpid(),last_child_pid);
+
+        close(1);
+        dup(fd[1]);
+        close(fd[1]);
+        if(single_execute(line)==-1){
+            exit_error("execvp #1 error");
+        }
+        exit(EXIT_SUCCESS);
+    }
+    //#4
+    close(fd[1]);
+    //#5-6
+    child2 = fork();
+    if (child2 == 0) {
+        set_default_signals();
+        //TODO- not sure that this is what they want
+        setpgid(getpid(),last_child_pid);
+        //child2 process
+        close(0);
+        dup(fd[0]);
+        close(fd[0]);
+        //check if there is a recursive call to execute
+        if(line->next->next!=0)
+            MultiPipeExecute(line->next,cur_job,job_list,last_child_pid);
+        if(single_execute(line->next)==-1)
+            exit_error("execvp #2 error");
+        exit(EXIT_SUCCESS);
+    }
+    //#7
+    close(fd[0]);
+
+    //#8
+    waitpid(child1, &cur_job->status, WUNTRACED);
+    waitpid(child2, &cur_job->status, WUNTRACED);
+    exit(EXIT_SUCCESS);
+}*/
+
+int main(int argc, char *argv[]) {
+    job *job_list = NULL;
+    //buf=getcwd input (getting the address)
+    char buf[PATH_MAX];
+    //userStream = used in fgets input
+    char userStream[2048];
+    int done = 0;
+    char *s;
+    //int cpid;
+    pid_t child_pid_t;
+    //lab 6 additions
+    //0a signals
+    signal(SIGQUIT, sig_handler);
+    signal(SIGCHLD, sig_handler);
+    //0c signals
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    //set group id
+    setpgid(getpid(), getpid());
+    struct termios *main_termios = (struct termios *) (malloc(sizeof(struct termios)));
+    tcgetattr(STDIN_FILENO, main_termios);
+    //job *tmpJob;
+    while (1) {
+        //get the current path
+        getcwd(buf, PATH_MAX);
+        printf("%s", buf);
+
+        //get the current command line
+        s = fgets(userStream, 2048, stdin);
+
+        //check \n case
+        if (strcmp(s, "\n") == 0 || strcmp(s, " \n") == 0 || strcmp(s, "    \n") == 0) {
+            strncpy(buf, buf + 1, strlen(buf) - 1);
+            getcwd(userStream, 2048);
+            printf("%s", userStream);
+            fgets(buf, 2048, stdin);
+            continue;
+        }
+
+        //if the execution order is quit ->exit 0
+        if (strcmp(s, "quit") == 0 || strcmp(s, "quit\n") == 0) {
+            if (job_list != NULL)
+                free_job_list(&job_list);
+            //exit(EXIT_SUCCESS);
+            break;
+        }
+        //check if the command is jobs-> if so- print as need, else- exec the command
+        if (strcmp(s, "jobs\n") == 0 || strcmp(s, "jobs") == 0) {
+            print_jobs(&job_list);
+            continue;
+        } else {
+            //else - go to parse the line and execute it
+            cmd_line *line = parse_cmd_lines(s);
+            add_job(&job_list, s);
+            done = initial_execute(line, &child_pid_t);
+            free_cmd_lines(line);
+        }
+        switch (done){
+            case EXIT_SUCCESS:
+                break;
+            case EXIT_FAILURE:
+                exit_error("runtime error");
+            default:
+                break;
+        }
+
+    }
+    if(job_list!=NULL)
+        free_job_list(&job_list);
+    return 0;
+}
+
+            /*
+            //else - go to parse the line and execute it
+            cmd_line *line = parse_cmd_lines(s);
+            tmpJob=add_job(&job_list, s);
+            //add the line to the job_line list
+            cpid = fork();
+            if (cpid == -1) {
+                exit_error("cpid -fork() error");
+            }
+            if (cpid == 0) {
+                //TODO - need to transfer the single execute into the multiExecute
+                //sent to execute in execute func, if -1 ->error
+                if (line->next == 0){
+
+                    done = single_execute(line);
+                }
+                    //else ->there is a next -> need a pipe module
+                else {
+                    line=parse_cmd_lines(tmpJob->cmd);
+                    //wrapper function will handle the initial call and waiting for the last child
+                    done = initial_execute(line, child_pid_t);
+                }
+                if (done == -1)
+                    exit_error("illegal execute line");
+                exit(0);
+            }
+            //check if there is a need to wait for son, if 0->don't wait
+            if (line->blocking == 1){
+                while(waitpid(tmpJob->pgid, &tmpJob->status, WUNTRACED)!= -1){};
+            }
+            //release the curr cmd_line from memory
+            free_cmd_lines(line);
+        }
+
+    }
+    if(job_list!=NULL)
+        free_job_list(&job_list);
+    return 0;
+
+
+}
+             */
---
+++ 1e/job_control.c
@@ -0,0 +1,234 @@
+#include "job_control.h"
+#define FREE(X) if(X) free((void*)X)
+
+/**
+* Receive a pointer to a job list and a new command to add to the job list and adds it to it.
+* Create a new job list if none exists.
+**/
+job* add_job(job** job_list, char* cmd){
+	job* job_to_add = initialize_job(cmd);
+	
+	if (*job_list == NULL){
+		*job_list = job_to_add;
+		job_to_add -> idx = 1;
+	}	
+	else{
+		int counter = 2;
+		job* list = *job_list;
+		while (list -> next !=NULL){
+			printf("adding %d\n", list->idx);
+			list = list -> next;
+			counter++;
+		}
+		job_to_add ->idx = counter;
+		list -> next = job_to_add;
+	}
+	return job_to_add;
+}
+
+
+/**
+* Receive a pointer to a job list and a pointer to a job and removes the job from the job list 
+* freeing its memory.
+**/
+void remove_job(job** job_list, job* tmp){
+	if (*job_list == NULL)
+		return;
+	job* tmp_list = *job_list;
+	if (tmp_list == tmp){
+		*job_list = tmp_list -> next;
+		free_job(tmp);
+		return;
+	}
+		
+	while (tmp_list->next != tmp){
+		tmp_list = tmp_list -> next;
+	}
+	tmp_list -> next = tmp -> next;
+	free_job(tmp);
+	
+}
+
+/**
+* receives a status and prints the string it represents.
+**/
+char* status_to_str(int status)
+{
+  static char* strs[] = {"Done", "Suspended", "Running"};
+  return strs[status + 1];
+}
+
+
+/**
+*   Receive a job list, and print it in the following format:<code>[idx] \t status \t\t cmd</code>, where:
+    cmd: the full command as typed by the user.
+    status: Running, Suspended, Done (for jobs that have completed but are not yet removed from the list).
+  
+**/
+void print_jobs(job** job_list){
+
+	job* tmp = *job_list;
+	update_job_list(job_list, FALSE);
+	while (tmp != NULL){
+		printf("[%d]\t %s \t\t %s", tmp->idx, status_to_str(tmp->status),tmp -> cmd); 
+		
+		if (tmp -> cmd[strlen(tmp -> cmd)-1]  != '\n')
+			printf("\n");
+		job* job_to_remove = tmp;
+		tmp = tmp -> next;
+		if (job_to_remove->status == DONE)
+			remove_job(job_list, job_to_remove);
+		
+	}
+ 
+}
+
+
+/**
+* Receive a pointer to a list of jobs, and delete all of its nodes and the memory allocated for each of them.
+*/
+void free_job_list(job** job_list){
+	while(*job_list != NULL){
+		job* tmp = *job_list;
+		*job_list = (*job_list) -> next;
+		free_job(tmp);
+	}
+	
+}
+
+
+/**
+* receives a pointer to a job, and frees it along with all memory allocated for its fields.
+**/
+void free_job(job* job_to_remove){
+    if(job_to_remove){
+        if(job_to_remove->tmodes)
+            FREE(job_to_remove->tmodes);
+        if(job_to_remove->cmd)
+            FREE(job_to_remove->cmd);
+        FREE(job_to_remove);
+    }
+}
+
+
+void str_copy (char* source, char* dest){
+    char* ptr = &source[0];
+    int idx = 0;
+    while (*ptr != '\0')
+        dest[idx++] = *ptr++;
+}
+
+
+/**
+* Receive a command (string) and return a job pointer. 
+* The function needs to allocate all required memory for: job, cmd, tmodes
+* to copy cmd, and to initialize the rest of the fields to NULL: next, pigd, status 
+**/
+
+job* initialize_job(char* cmd){
+	job* output =malloc(sizeof(job));
+	output->cmd=malloc(1024*sizeof(char));
+	//TODO
+	str_copy(cmd,output->cmd);
+    //strcpy(output->cmd,cmd);
+    output->idx = 1;
+	output->tmodes=malloc(sizeof(struct termios));
+	output->next=NULL;
+	output->pgid=0;
+	output->status=RUNNING;
+	return output;
+}
+
+
+/**
+* Receive a job list and and index and return a pointer to a job with the given index, according to the idx field.
+* Print an error message if no job with such an index exists.
+**/
+job* find_job_by_index(job* job_list, int idx){
+  while(job_list!=NULL){
+      if(job_list->idx==idx)
+          return job_list;
+      job_list=job_list->next;
+  }
+  //if got here-> there is no idx like the input-> return null
+  return NULL;
+}
+
+
+/**
+* Receive a pointer to a job list, and a boolean to decide whether to remove done
+* jobs from the job list or not. 
+**/
+void update_job_list(job **job_list, int remove_done_jobs){
+    job* tmp = *job_list;
+    while(tmp!=NULL){
+        waitpid(tmp->pgid, &tmp->status, WNOHANG);
+        if(remove_done_jobs){
+            if(tmp->status==DONE){
+                printf("[%d]\t %s \t\t %s", tmp->idx, status_to_str(tmp->status),tmp -> cmd);
+                if (tmp -> cmd[strlen(tmp -> cmd)-1]  != '\n')
+                    printf("\n");
+                job* job_to_remove = tmp;
+                remove_job(job_list,job_to_remove);
+            }
+        }
+        tmp=tmp->next;
+    }
+
+}
+
+/** 
+* Put job j in the foreground.  If cont is nonzero, restore the saved terminal modes and send the process group a
+* SIGCONT signal to wake it up before we block.  Run update_job_list to print DONE jobs.
+**/
+//task 1d
+void run_job_in_foreground (job** job_list, job *j, int cont, struct termios* shell_tmodes, pid_t shell_pgid){
+ int job_status;
+ job* tmp = *job_list;
+ if(waitpid(j->pgid,&job_status,WNOHANG)==-1)
+     j->status=DONE;
+ if(j->status==DONE){
+     //if DONE->do the procedure like in print_job
+     printf("[%d]\t %s \t\t %s", tmp->idx, status_to_str(tmp->status),tmp -> cmd);
+     if (tmp -> cmd[strlen(tmp -> cmd)-1]  != '\n')
+         printf("\n");
+     remove_job(job_list,j);
+ }
+ //else- the job hasn't done
+ else{
+     tcsetpgrp (STDIN_FILENO,j->pgid);
+     if(cont==1 &&j->status==SUSPENDED){
+         tcsetattr (STDIN_FILENO, TCSADRAIN, j->tmodes);
+     }
+     kill(j->pgid,SIGCONT);
+     //2 signals in the last arguments- will catch the relevant one
+     waitpid(j->pgid,&job_status,WUNTRACED | WSTOPPED);
+     if(WIFSTOPPED(job_status))
+         //TODO-maybe need to prinf("WIFSTOPPED");
+         j->status=SUSPENDED;
+     else{
+         if(WIFSIGNALED(job_status) || WIFEXITED((job_status)))
+             j->status=DONE;
+     }
+ }
+
+ //occurs when the shell returns from waitpid
+    tcsetpgrp(STDIN_FILENO,shell_pgid); //Put the shell back in the foreground
+    tcgetattr(STDIN_FILENO,j->tmodes);//Save the terminal attributes in the job tmodes
+    tcsetattr(STDIN_FILENO,TCSADRAIN,shell_tmodes);//Restore the shell’s terminal attributes using the shell tmodes
+    //Check for status update of jobs that are running in the background
+    update_job_list(job_list,1);
+}
+
+/** 
+* Put a job in the background.  If the cont argument is nonzero, send
+* the process group a SIGCONT signal to wake it up.  
+**/
+//task 1e
+void run_job_in_background (job *j, int cont){	
+ job* tmp=find_job_by_index(j,cont);
+ if(tmp==NULL)
+     exit(EXIT_FAILURE);
+ tmp->status=RUNNING;
+ kill(tmp->pgid,SIGCONT);
+}
